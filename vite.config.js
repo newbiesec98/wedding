9c0bd9ae067f6c283acd9ad78a0eee47bc73d758
AZMI-PC <azmimaru15@gmail.com>
2026-04-20 22:09:51 +0800

    update Domain

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     host: true,
     port: 5175,
-    allowedHosts: ['sopian.nurulaitam.my.id', 'sofianhariyanti.nurulaitam.my.id'],
+    allowedHosts: ['sopian.nurulaitam.my.id', 'sofianhariyanti.nurulaitam.my.id', 'wedding.nurulaitam.my.id'],
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
